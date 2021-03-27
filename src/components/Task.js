0a52f0c842d64510036cc8821824c9661a7a9ca0
Grace Lindelien <gracelindelien@gmail.com>
2021-03-27 14:11:23 -0700

    Add ability to toggle strikethrough on click

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,11 @@
-const Task = ({ data }) => {
+import '../css/task.css';
+
+const Task = ({ task, index, toggleCompletion }) => {
+
+  let className = task.complete ? 'complete' : null;
+
   return (
-    <li>{data}</li>
+    <li onClick={() => {toggleCompletion(index)}} className={className}>{task.data}</li>
   );
 };
 
--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -7,17 +7,29 @@ const ToDoApp = () => {
 
   const [tasks, setTasks] = useLocalStorage('tasks', []);
 
-  const addTask = (task) => {
-    setTasks([...tasks, task]);
+  const addTask = (data) => {
+    setTasks([...tasks, {data, complete: false}]);
   }
+
+  const toggleCompletion = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].complete = !updatedTasks[index].complete;
+    setTasks(updatedTasks);
+  };
 
   return (
     <div>
+
       <Form addTask={addTask} />
+
       <ul>
-        {tasks.map((current, index) => <Task data={current} key={index}/>)}
+        {tasks.map((current, index) => <Task task={current} key={index} index={index} toggleCompletion={toggleCompletion} />)}
       </ul>
-      <input type="button" value="Clear All Tasks" onClick={() => {setTasks([])}}></input>
+
+      <input type="button"
+             value="Clear All Tasks"
+             onClick={() => {setTasks([])}} />
+
     </div>
   );
 
